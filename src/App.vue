79fd68e03478cf6accea7d523a199fc2cb886327
cleDiego <cleversondesign@hotmail.com>
2022-03-01 16:03:53 -0300

    Buttons

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <Button type="success" text="Button"/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Button from './components/Button'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Button
   }
 }
 </script>
---
+++ src/components/Button.vue
@@ -0,0 +1,18 @@
+
+
+<template>
+  <button type="button" class="btn btn-outline-primary">{{text}}</button>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    text: String,
+    type: String
+  }
+}
+</script>
+
+<style scoped>
+</style>
